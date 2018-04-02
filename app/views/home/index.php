dd686f250a3ff49c4a14ecd11ab57bdba4b36a66
TheBinaryLoop <TheBinaryLoop@users.noreply.github.com>
2018-04-02 16:29:57 +0200

    Create index.php

---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ozon Framework</title>
+    </head>
+    <body>
+        <h1>Welcome to the demo page</h1>
+        <p>You are at home::index</p>
+    </body>
+</html>
